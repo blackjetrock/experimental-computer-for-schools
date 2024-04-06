c6e20de0d6a300038331975aa6755cb1434b46ec
blackrockjet <menadue@gmail.com>
2024-04-06 06:49:02 +0100

    All tests pass

--- firmware/pico_main/switches.h
+++ firmware/pico_main/switches.h
@@ -1,12 +1,12 @@
 #define DEBUG_CALC_STATE       1
 #define DEBUG_A_DECODE         0
 #define DEBUG_LOOP             0
-#define DEBUG_BCD_CORRECTION   1
+#define DEBUG_BCD_CORRECTION   0
 #define DEBUG_TEST             1    // Test instruction
-#define DEBUG_SW_BCD_SUM       1
+#define DEBUG_SW_BCD_SUM       0
 #define DEBUG_TEST_SEQ         1    // Test sequences
-#define DEBUG_SERIAL           1
-#define DEBUG_KEY_SCAN         1
+#define DEBUG_SERIAL           0
+#define DEBUG_KEY_SCAN         0
 
 #define OLED_ON                1
 #define SD_ON                  1
