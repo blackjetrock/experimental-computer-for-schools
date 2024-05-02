16057f08eb87fa99b68a93592a0775b8750a5a2c
blackrockjet <menadue@gmail.com>
2024-05-02 18:47:16 +0100

    Updates

---
+++ firmware/touch_kb/tkb.c
@@ -0,0 +1,84 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Experimental Schools Computer Simulator
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Toouch Keyboard
+//
+// Runs on RP Pico
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+
+#include <ctype.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/irq.h"
+#include "hardware/flash.h"
+#include "hardware/structs/bus_ctrl.h"
+
+#define DEBUG_STOP while(1) {}
+
+// Some logic to analyse:
+#include "hardware/structs/pwm.h"
+#include "pico/multicore.h"
+#include "pico/bootrom.h"
+
+
+int main(void)
+{
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  // Overclock as needed
+  //
+  ////////////////////////////////////////////////////////////////////////////////
+  
+#define OVERCLOCK 135000
+  //#define OVERCLOCK 200000
+  //#define OVERCLOCK 270000
+  //#define OVERCLOCK 360000
+  
+#if OVERCLOCK > 270000
+  /* Above this speed needs increased voltage */
+  vreg_set_voltage(VREG_VOLTAGE_1_20);
+  sleep_ms(1000);
+#endif
+  
+  /* Overclock */
+  set_sys_clock_khz( OVERCLOCK, 1 );
+
+  stdio_init_all();
+
+  sleep_ms(2000);
+
+  printf("\n");
+  printf("\n                                  ********************************************");
+  printf("\n                                  * Experimental Schools Computer Simulation *");
+  printf("\n                                  ********************************************");
+  printf("\n");
+
+
+  
+  // Set up GPIOs
+
+#define DRV 2
+  
+  gpio_init(DRV);
+  gpio_set_dir(DRV, GPIO_OUT);
+
+  while(1)
+    {
+      gpio_put(DRV, 1);
+      gpio_put(DRV, 0);
+    }
+}
